f8c0245ea11a8e2fdb644514fcfdfbf91c9aa8fd
Kirsten Allen <kmaaallen@gmail.com>
2019-01-07 19:35:56 +0000

    added getQuestion function to display questions and options

--- js/script.js
+++ js/script.js
@@ -11,16 +11,36 @@ var optionB = document.getElementById("B");
 var optionC = document.getElementById("C");
 var optionD = document.getElementById("D");
 
-var score = 0
+var score = 0;
+
+//questions function so our getQuestion function later can get the right value from array
+
+function Questions (question,options,answer){
+    this.question = question;
+    this.options=options;
+    this.answer=answer;
+}
 
 var questions = [
     new Questions("Which bird is a Korimako?", ["New Zealand Falcon", "Bellbird", "Rock wren", "Pigeon"], "B"),
     new Questions("Which bird is a Pīwakawaka?", ["Saddleback", "Sparrow", "Fantail", "Stitchbird"], "C"),
     new Questions("Which bird is a Ruru?", ["Morepork", "Whitehead", "Wax-eye", "New Zealand Parakeet"], "A"),
-    ]   
+    ]   ;
+    
+var questionIndex = 0;
+var finalQuestion = questions.length - 1;
 
-//start function to begin the quiz
+// getQuestion function
 
+function getQuestion(){
+    let q = questions[questionIndex];
+    question.innerHTML = "<p>"+ q.question + "</p>";
+    optionA.innerHTML = "<p>"+ q.options[0] + "</p>";
+    optionB.innerHTML = "<p>"+ q.options[1] + "</p>";
+    optionC.innerHTML = "<p>"+ q.options[2] + "</p>";
+    optionD.innerHTML = "<p>"+ q.options[3] + "</p>";
+    
+}
 
     
 
